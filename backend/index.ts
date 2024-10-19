b7b2674e074e6aa19e4e5421515bcf87a1f3316f
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-10-19 08:57:45 +0600

    init my backend

---
+++ backend/index.ts
@@ -0,0 +1,27 @@
+import cors from 'cors';
+import express from 'express';
+import  mongoose from "mongoose";
+import config from "./config";
+import path from "path";
+
+
+const app = express();
+const port = 8030;
+
+
+app.use(express.static(path.join(config.publicPath, 'images')));
+app.use(cors());
+app.use(express.json());
+app.use('/users',usersRouter);
+
+
+const run = async () => {
+    await mongoose.connect(config.db);
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+};
+run().catch(console.error);
